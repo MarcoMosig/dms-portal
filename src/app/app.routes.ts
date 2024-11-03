60f31ea832be9443a56ee6efcc490aef332072da
Marco Mosig <m.mosig@2m-software.de>
2024-11-03 14:52:26 +0100

    add dashboards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,15 @@ import {LayoutComponent} from "app/layout/layout.component";
 import {NoAuthGuard} from "app/core/auth/guards/noAuth.guard";
 
 export const appRoutes: Routes = [
+  // Redirect empty path to '/dashboards/project'
+  {path: '', pathMatch : 'full', redirectTo: 'dashboards/project'},
+
+  // Redirect signed-in user to the '/dashboards/project'
+  //
+  // After the user signs in, the sign-in page will redirect the user to the 'signed-in-redirect'
+  // path. Below is another redirection for that path to redirect the user to the desired
+  // location. This is a small convenience to keep all main routes together here on this file.
+  {path: 'signed-in-redirect', pathMatch : 'full', redirectTo: 'dashboards/project'},
   // Auth routes for guests
   {
     path: '',
